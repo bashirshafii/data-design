916e47130d3bab650414922e7bf1c0374f5ab83a
Bashir Shafii <bashir.shafii@gmail.com>
2018-07-23 17:07:01 -0500

    updated file structure for autoload php

--- php/classes/Article.php
+++ php/classes/Article.php
@@ -1,7 +1,7 @@
 <?php
 namespace BashirShafii\DataDesign;
 require_once("autoload.php");
-require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+require_once(dirname(__DIR__, 2) . "../vendor/autoload.php");
 use Ramsey\Uuid\Uuid;
 /**
  * Newspaper article class
